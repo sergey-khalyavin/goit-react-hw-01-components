a0cb6e44f026508df6e05d10d81e923cf9361b1f
Sergey Khalyavin <serg.khalk@gmail.com>
2020-09-11 18:13:06 +0300

    Updates

---
+++ precache-manifest.a535c751576527813067b8a6c4e0b2f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c18915a1abde8fa69973319559f65fff",
+    "url": "/sergey-khalyavin/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "516ddf47e0dbd175b7f2",
+    "url": "/sergey-khalyavin/goit-react-hw-01-components/static/css/main.14587413.chunk.css"
+  },
+  {
+    "revision": "8d9dc58c1a4c85358da3",
+    "url": "/sergey-khalyavin/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sergey-khalyavin/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "516ddf47e0dbd175b7f2",
+    "url": "/sergey-khalyavin/goit-react-hw-01-components/static/js/main.6b132265.chunk.js"
+  },
+  {
+    "revision": "8d611afcec45c524e785",
+    "url": "/sergey-khalyavin/goit-react-hw-01-components/static/js/runtime-main.85eac9ec.js"
+  }
+]);
